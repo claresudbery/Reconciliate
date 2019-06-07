f8f7184e4ea2ebcd8c7844da309856166f92e5f9
claresudbery <clareworking@claresudbery.co.uk>
2019-06-07 22:35:55 +0100

    Missed an instruction  string

--- Console/Reconciliation/ReconciliationIntro.cs
+++ Console/Reconciliation/ReconciliationIntro.cs
@@ -256,8 +256,8 @@ namespace ConsoleCatchall.Console.Reconciliation
             _inputOutput.OutputLine("***              The working copy will be placed here:              ***");
             _inputOutput.OutputLine($"***  {ReconConsts.MainSpreadsheetPath}/{ReconConsts.BackupSubFolder}  ***");
             _inputOutput.OutputLine("***                                                                 ***");
-            _inputOutput.OutputLine("***        You can find debug versions of BankOutPending.csv        ***");
-            _inputOutput.OutputLine($"***            and Bank.csv in {ReconConsts.DefaultFilePath}/DEBUG              ***");
+            _inputOutput.OutputLine("***  You can find debug versions of all csv files and a spreadsheet ***");
+            _inputOutput.OutputLine($"***     in [project root]/reconciliation-samples/For debugging     ***");
             _inputOutput.OutputLine("***                                                                 ***");
             _inputOutput.OutputLine("***********************************************************************");
             _inputOutput.OutputLine("***********************************************************************");
