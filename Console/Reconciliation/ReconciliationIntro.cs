3f00ebcee37ba72dac2be9a2b11698e39e82d82c
claresudbery <clareworking@claresudbery.co.uk>
2019-06-22 18:31:52 +0100

    Fixed and added tests for intermittent bug.

--- Console/Reconciliation/ReconciliationIntro.cs
+++ Console/Reconciliation/ReconciliationIntro.cs
@@ -465,12 +465,21 @@ namespace ConsoleCatchall.Console.Reconciliation
                     main_spreadsheet_path, 
                     ReconConsts.Backup_sub_folder,
                     ReconConsts.Debug_spreadsheet_file_name);
-                File.Copy(source_file_path, debug_file_path, true);
+                Copy_file(source_file_path, debug_file_path);
             }
             else
             {
                 throw new Exception($"Can't find file: {source_file_path}");
             }
+        }
+
+        private void Copy_file(string source_file_path, string dest_file_path)
+        {
+            // There seems to be an intermittent fault in Windows - sometimes it copes with mixed slashes and sometimes it doesn't!
+            source_file_path = source_file_path.Replace("/", "\\");
+            dest_file_path = dest_file_path.Replace("/", "\\");
+
+            File.Copy(source_file_path, dest_file_path, true);
         }
 
         public void Create_backup_of_real_spreadsheet(IClock clock, string spreadsheet_path)
@@ -484,7 +493,7 @@ namespace ConsoleCatchall.Console.Reconciliation
                 string backup_file_name = file_name_prefix + "_" + ReconConsts.Main_spreadsheet_file_name;
                 string backup_file_path = spreadsheet_path + "\\" + backup_file_name;
 
-                File.Copy(source_file_path, backup_file_path, true);
+                Copy_file(source_file_path, backup_file_path);
             }
             else
             {
