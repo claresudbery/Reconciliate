c323774fbe4f4bf760d955efa07b2548df5016de
claresudbery <clareworking@claresudbery.co.uk>
2019-06-23 17:03:06 +0100

    Copied all the generic methods up into the `FileLoader` class, called them from the main parent method and injected the specific loader object into them.

--- Console/Reconciliation/Loaders/FileLoader.cs
+++ Console/Reconciliation/Loaders/FileLoader.cs
@@ -3,6 +3,7 @@ using ConsoleCatchall.Console.Reconciliation.Files;
 using ConsoleCatchall.Console.Reconciliation.Records;
 using ConsoleCatchall.Console.Reconciliation.Spreadsheets;
 using Interfaces;
+using Interfaces.Constants;
 using Interfaces.DTOs;
 
 namespace ConsoleCatchall.Console.Reconciliation.Loaders
@@ -59,7 +60,7 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                             var owned_file_io = new FileIO<BankRecord>(_spreadsheet_factory);
                             var data_loading_info = BankAndBankInData.LoadingInfo;
                             var loader = new BankAndBankInLoader(_input_output, _spreadsheet_factory);
-                            reconciliation_interface = loader.Load(
+                            reconciliation_interface = Load(
                                     spreadsheet,
                                     budgeting_months,
                                     main_file_paths,
@@ -67,7 +68,8 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                                     pending_file,
                                     third_party_file_io,
                                     owned_file_io,
-                                    data_loading_info);
+                                    data_loading_info,
+                                    loader);
                         }
                         break;
                     case ReconciliationType.BankAndBankOut:
@@ -78,7 +80,7 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                             var owned_file_io = new FileIO<BankRecord>(_spreadsheet_factory);
                             var data_loading_info = BankAndBankOutData.LoadingInfo;
                             var loader = new BankAndBankOutLoader(_input_output, _spreadsheet_factory);
-                            reconciliation_interface = loader.Load(
+                            reconciliation_interface = Load(
                                     spreadsheet,
                                     budgeting_months,
                                     main_file_paths,
@@ -86,7 +88,8 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                                     pending_file,
                                     third_party_file_io,
                                     owned_file_io,
-                                    data_loading_info);
+                                    data_loading_info,
+                                    loader);
                         }
                         break;
                     case ReconciliationType.CredCard1AndCredCard1InOut:
@@ -97,7 +100,7 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                             var owned_file_io = new FileIO<CredCard1InOutRecord>(_spreadsheet_factory);
                             var data_loading_info = CredCard1AndCredCard1InOutData.LoadingInfo;
                             var loader = new CredCard1AndCredCard1InOutLoader(_input_output, _spreadsheet_factory);
-                            reconciliation_interface = loader.Load(
+                            reconciliation_interface = Load(
                                     spreadsheet,
                                     budgeting_months,
                                     main_file_paths,
@@ -105,7 +108,8 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                                     pending_file,
                                     third_party_file_io,
                                     owned_file_io,
-                                    data_loading_info);
+                                    data_loading_info,
+                                    loader);
                         }
                         break;
                     case ReconciliationType.CredCard2AndCredCard2InOut:
@@ -116,7 +120,7 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                             var owned_file_io = new FileIO<CredCard2InOutRecord>(_spreadsheet_factory);
                             var data_loading_info = CredCard2AndCredCard2InOutData.LoadingInfo;
                             var loader = new CredCard2AndCredCard2InOutLoader(_input_output, _spreadsheet_factory);
-                            reconciliation_interface = loader.Load(
+                            reconciliation_interface = Load(
                                     spreadsheet,
                                     budgeting_months,
                                     main_file_paths,
@@ -124,7 +128,8 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                                     pending_file,
                                     third_party_file_io,
                                     owned_file_io,
-                                    data_loading_info);
+                                    data_loading_info,
+                                    loader);
                         }
                         break;
                     default:
@@ -143,6 +148,138 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
             _input_output.Output_line("");
 
             return reconciliation_interface;
+        }
+
+        public ReconciliationInterface Load<TThirdPartyType, TOwnedType>(
+                ISpreadsheet spreadsheet,
+                BudgetingMonths budgeting_months,
+                FilePaths main_file_paths,
+                IFileIO<TOwnedType> pending_file_io,
+                ICSVFile<TOwnedType> pending_file,
+                IFileIO<TThirdPartyType> third_party_file_io,
+                IFileIO<TOwnedType> owned_file_io,
+                DataLoadingInformation data_loading_info,
+                ILoader loader)
+            where TThirdPartyType : ICSVRecord, new()
+            where TOwnedType : ICSVRecord, new()
+        {
+            data_loading_info.File_paths = main_file_paths;
+            Load_pending_data(pending_file_io, pending_file, data_loading_info);
+            Merge_budget_data(spreadsheet, budgeting_months, pending_file, data_loading_info);
+            Merge_other_data(spreadsheet, budgeting_months, pending_file, data_loading_info, loader);
+            Merge_unreconciled_data(spreadsheet, pending_file, data_loading_info);
+            var reconciliator = Load_third_party_and_owned_files_into_reconciliator(third_party_file_io, owned_file_io, data_loading_info);
+            var reconciliation_interface = Create_reconciliation_interface(reconciliator, data_loading_info);
+
+            return reconciliation_interface;
+        }
+
+        private ReconciliationInterface Create_reconciliation_interface<TThirdPartyType, TOwnedType>(
+                Reconciliator<TThirdPartyType, TOwnedType> reconciliator,
+                DataLoadingInformation data_loading_info)
+            where TThirdPartyType : ICSVRecord, new()
+            where TOwnedType : ICSVRecord, new()
+        {
+            _input_output.Output_line(ReconConsts.CreatingReconciliationInterface);
+            return new ReconciliationInterface(
+                _input_output,
+                reconciliator,
+                data_loading_info.Third_party_descriptor,
+                data_loading_info.Owned_file_descriptor);
+        }
+
+        private Reconciliator<TThirdPartyType, TOwnedType> Load_third_party_and_owned_files_into_reconciliator<TThirdPartyType, TOwnedType>(
+                IFileIO<TThirdPartyType> third_party_file_io,
+                IFileIO<TOwnedType> owned_file_io,
+                DataLoadingInformation data_loading_info)
+            where TThirdPartyType : ICSVRecord, new()
+            where TOwnedType : ICSVRecord, new()
+        {
+            _input_output.Output_line(ReconConsts.LoadingDataFromFiles);
+            third_party_file_io.Set_file_paths(data_loading_info.File_paths.Main_path,
+                data_loading_info.File_paths.Third_party_file_name);
+            owned_file_io.Set_file_paths(data_loading_info.File_paths.Main_path, data_loading_info.File_paths.Owned_file_name);
+
+            var reconciliator = new Reconciliator<TThirdPartyType, TOwnedType>(
+                third_party_file_io,
+                owned_file_io,
+                data_loading_info);
+            return reconciliator;
+        }
+
+        private void Merge_unreconciled_data<TOwnedType>(
+                ISpreadsheet spreadsheet,
+                ICSVFile<TOwnedType> pending_file,
+                DataLoadingInformation data_loading_info)
+            where TOwnedType : ICSVRecord, new()
+        {
+            // Pending file will already exist, having already been split out from phone Notes file by a separate function call.
+            // We loaded it up into memory in the previous file-specific method.
+            // Then some budget amounts were added to that file (in memory).
+            // Other budget amounts (like CredCard1 balance) were written directly to the spreadsheet before this too.
+            // Now we load the unreconciled rows from the spreadsheet and merge them with the pending and budget data.
+            // Then we write all that data away into the 'owned' csv file (eg BankOutPending.csv).
+            _input_output.Output_line(ReconConsts.MergingUnreconciledRows);
+            spreadsheet.Add_unreconciled_rows_to_csv_file(data_loading_info.Sheet_name, pending_file);
+
+            _input_output.Output_line(ReconConsts.CopyingMergedData);
+            pending_file.Update_source_lines_for_output(data_loading_info.Loading_separator);
+            pending_file.Write_to_file_as_source_lines(data_loading_info.File_paths.Owned_file_name);
+
+            _input_output.Output_line(ReconConsts.StuffIsHappening);
+        }
+
+        private void Merge_other_data<TOwnedType>(
+                ISpreadsheet spreadsheet,
+                BudgetingMonths budgeting_months,
+                ICSVFile<TOwnedType> pending_file,
+                DataLoadingInformation data_loading_info,
+                ILoader loader)
+            where TOwnedType : ICSVRecord, new()
+        {
+            _input_output.Output_line(ReconConsts.MergingBespokeData);
+            loader.Merge_bespoke_data_with_pending_file(
+                _input_output,
+                spreadsheet,
+                pending_file,
+                budgeting_months,
+                data_loading_info);
+        }
+
+        private void Merge_budget_data<TOwnedType>(
+                ISpreadsheet spreadsheet,
+                BudgetingMonths budgeting_months,
+                ICSVFile<TOwnedType> pending_file,
+                DataLoadingInformation data_loading_info)
+            where TOwnedType : ICSVRecord, new()
+        {
+            _input_output.Output_line(ReconConsts.MergingBudgetDataWithPendingData);
+            spreadsheet.Add_budgeted_monthly_data_to_pending_file(
+                budgeting_months,
+                pending_file,
+                data_loading_info.Monthly_budget_data);
+            if (null != data_loading_info.Annual_budget_data)
+            {
+                spreadsheet.Add_budgeted_annual_data_to_pending_file(
+                    budgeting_months,
+                    pending_file,
+                    data_loading_info.Annual_budget_data);
+            }
+        }
+
+        private void Load_pending_data<TOwnedType>(
+                IFileIO<TOwnedType> pending_file_io,
+                ICSVFile<TOwnedType> pending_file,
+                DataLoadingInformation data_loading_info)
+            where TOwnedType : ICSVRecord, new()
+        {
+            _input_output.Output_line(ReconConsts.LoadingDataFromPendingFile);
+            pending_file_io.Set_file_paths(data_loading_info.File_paths.Main_path, data_loading_info.Pending_file_name);
+            pending_file.Load(true, data_loading_info.Default_separator);
+            // The separator we loaded with had to match the source. Then we convert it here to match its destination.
+            _input_output.Output_line(ReconConsts.ConvertingSourceLineSeparators);
+            pending_file.Convert_source_line_separators(data_loading_info.Default_separator,
+                data_loading_info.Loading_separator);
         }
     }
 }
