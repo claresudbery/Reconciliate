c7c495de97a4ba3fda2a738e9e018668d13b21af
claresudbery <clareworking@claresudbery.co.uk>
2019-11-28 18:01:07 +0000

    Branch syncing: Added Matcher member to FilePaths class

--- Interfaces/DTOs/FilePaths.cs
+++ Interfaces/DTOs/FilePaths.cs
@@ -2,6 +2,7 @@ namespace Interfaces.DTOs
 {
     public class FilePaths
     {
+        public IMatcher Matcher;
         public string Main_path { get; set; }
         public string Third_party_file_name { get; set; }
         public string Owned_file_name { get; set; }
