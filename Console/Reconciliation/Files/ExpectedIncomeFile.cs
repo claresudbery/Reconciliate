00a6815bb543fa584ff22010328626c09f0217e7
Clare Sudbery <csudbery@etsy.com>
2019-12-12 00:50:42 +0000

    Write ExpectedIn spreadsheet data to csv file.

--- Console/Reconciliation/Files/ExpectedIncomeFile.cs
+++ Console/Reconciliation/Files/ExpectedIncomeFile.cs
@@ -71,6 +71,7 @@ namespace ConsoleCatchall.Console.Reconciliation.Files
                 matched_record.Reconcile();
             }
             File.Write_back_to_main_spreadsheet(MainSheetNames.Expected_in);
+            File.Write_to_csv_file("recon");
         }
     }
 }
