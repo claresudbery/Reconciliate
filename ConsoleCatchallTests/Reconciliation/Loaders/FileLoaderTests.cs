78ff93d3250992d947987110171b5e769eb28de5
Clare Sudbery <clareworking@claresudbery.co.uk>
2019-12-18 21:55:57 +0000

    Reinstated accidentally-deleted test.

--- ConsoleCatchallTests/Reconciliation/Loaders/FileLoaderTests.cs
+++ ConsoleCatchallTests/Reconciliation/Loaders/FileLoaderTests.cs
@@ -253,5 +253,60 @@ namespace ConsoleCatchallTests.Reconciliation.Loaders
             Assert_pending_record_is_given_the_specified_direct_debit_details(pending_records[0], next_direct_debit_date01, expected_amount1, ReconConsts.Cred_card1_regular_pymt_description);
             Assert_pending_record_is_given_the_specified_direct_debit_details(pending_records[1], next_direct_debit_date02, expected_amount2, ReconConsts.Cred_card1_regular_pymt_description);
         }
+
+        [Test]
+        public void Cred_card1_and_cred_card1_in_out__Merge_bespoke_data_with_pending_file__Will_update_CredCard1_balances_on_totals_sheet()
+        {
+            // Arrange
+            TestHelper.Set_correct_date_formatting();
+            var mock_input_output = new Mock<IInputOutput>();
+            double new_balance = 5673.99;
+            DateTime last_direct_debit_date = new DateTime(2018, 12, 17);
+            var next_direct_debit_date01 = last_direct_debit_date.AddMonths(1);
+            var next_direct_debit_date02 = last_direct_debit_date.AddMonths(2);
+            mock_input_output
+                .Setup(x => x.Get_input(
+                    string.Format(
+                        ReconConsts.AskForCredCardDirectDebit,
+                        ReconConsts.Cred_card1_name,
+                        next_direct_debit_date01.ToShortDateString()), ""))
+                .Returns(new_balance.ToString);
+            mock_input_output
+                .Setup(x => x.Get_input(
+                    string.Format(
+                        ReconConsts.AskForCredCardDirectDebit,
+                        ReconConsts.Cred_card1_name,
+                        next_direct_debit_date02.ToShortDateString()), ""))
+                .Returns("0");
+            var bank_record = new BankRecord { Date = last_direct_debit_date };
+            var mock_spreadsheet = new Mock<ISpreadsheet>();
+            mock_spreadsheet.Setup(x => x.Get_most_recent_row_containing_text<BankRecord>(
+                    MainSheetNames.Bank_out, ReconConsts.Cred_card1_dd_description, new List<int> { ReconConsts.DescriptionColumn, ReconConsts.DdDescriptionColumn }))
+                .Returns(bank_record);
+            var mock_pending_file = new Mock<ICSVFile<CredCard1InOutRecord>>();
+            var pending_records = new List<CredCard1InOutRecord>();
+            mock_pending_file.Setup(x => x.Records).Returns(pending_records);
+            var budgeting_months = new BudgetingMonths();
+            var loading_info = CredCard1AndCredCard1InOutData.LoadingInfo;
+            var file_loader = new FileLoader();
+
+            // Act
+            file_loader.Cred_card1_and_cred_card1_in_out__Merge_bespoke_data_with_pending_file(
+                mock_input_output.Object,
+                mock_spreadsheet.Object,
+                mock_pending_file.Object,
+                budgeting_months,
+                loading_info);
+
+            // Assert
+            mock_spreadsheet.Verify(x => x.Update_balance_on_totals_sheet(
+                Codes.Cred_card1_bal,
+                new_balance * -1,
+                string.Format(
+                    ReconConsts.CredCardBalanceDescription,
+                    ReconConsts.Cred_card1_name,
+                    $"{last_direct_debit_date.ToString("MMM")} {last_direct_debit_date.Year}"),
+                5, 6, 4), Times.Exactly(1));
+        }
     }
 }
