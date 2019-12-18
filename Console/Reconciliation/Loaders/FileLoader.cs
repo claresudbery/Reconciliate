034147661aeb13fc8de12aa9e3f04c63610755e1
Clare Sudbery <clareworking@claresudbery.co.uk>
2019-12-18 18:06:50 +0000

    Added a new FileLoader class and copied method ready for calling.

---
+++ Console/Reconciliation/Loaders/FileLoader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using ConsoleCatchall.Console.Reconciliation.Records;
+using ConsoleCatchall.Console.Reconciliation.Spreadsheets;
+using Interfaces;
+using Interfaces.Constants;
+
+namespace ConsoleCatchall.Console.Reconciliation.Loaders
+{
+    internal class FileLoader
+    {
+        public void Bank_and_bank_out__Add_most_recent_credit_card_direct_debits(
+            IInputOutput input_output,
+            ISpreadsheet spreadsheet,
+            ICSVFile<BankRecord> pending_file,
+            string cred_card_name,
+            string direct_debit_description)
+        {
+            var most_recent_cred_card_direct_debit = spreadsheet.Get_most_recent_row_containing_text<BankRecord>(
+                MainSheetNames.Bank_out,
+                direct_debit_description,
+                new List<int> { ReconConsts.DescriptionColumn, ReconConsts.DdDescriptionColumn });
+
+            var next_date = most_recent_cred_card_direct_debit.Date.AddMonths(1);
+            var input = input_output.Get_input(string.Format(
+                ReconConsts.AskForCredCardDirectDebit,
+                cred_card_name,
+                next_date.ToShortDateString()));
+            while (input != "0")
+            {
+                double amount;
+                if (double.TryParse(input, out amount))
+                {
+                    pending_file.Records.Add(new BankRecord
+                    {
+                        Date = next_date,
+                        Description = direct_debit_description,
+                        Type = "POS",
+                        Unreconciled_amount = amount
+                    });
+                }
+                next_date = next_date.Date.AddMonths(1);
+                input = input_output.Get_input(string.Format(
+                    ReconConsts.AskForCredCardDirectDebit,
+                    cred_card_name,
+                    next_date.ToShortDateString()));
+            }
+        }
+    }
+}
