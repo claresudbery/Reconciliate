3bdf8674489d21f23887344718af98ddbb5a379f
Clare Sudbery <clareworking@claresudbery.co.uk>
2019-12-18 22:07:03 +0000

    Moved Cred_card2_and_cred_card2_in_out__Merge_bespoke_data_with_pending_file from ReconciliationIntro to FileLoader.

--- Console/Reconciliation/Loaders/FileLoader.cs
+++ Console/Reconciliation/Loaders/FileLoader.cs
@@ -139,5 +139,55 @@ namespace ConsoleCatchall.Console.Reconciliation.Loaders
                 text_column: 6,
                 code_column: 4);
         }
+
+        public void Cred_card2_and_cred_card2_in_out__Merge_bespoke_data_with_pending_file(
+                IInputOutput input_output,
+                ISpreadsheet spreadsheet,
+                ICSVFile<CredCard2InOutRecord> pending_file,
+                BudgetingMonths budgeting_months,
+                DataLoadingInformation data_loading_info)
+        {
+            var most_recent_cred_card_direct_debit = spreadsheet.Get_most_recent_row_containing_text<BankRecord>(
+                MainSheetNames.Bank_out,
+                ReconConsts.Cred_card2_dd_description,
+                new List<int> { ReconConsts.DescriptionColumn, ReconConsts.DdDescriptionColumn });
+
+            var statement_date = new DateTime();
+            var next_date = most_recent_cred_card_direct_debit.Date.AddMonths(1);
+            var input = input_output.Get_input(string.Format(
+                ReconConsts.AskForCredCardDirectDebit,
+                ReconConsts.Cred_card2_name,
+                next_date.ToShortDateString()));
+            double new_balance = 0;
+            while (input != "0")
+            {
+                if (double.TryParse(input, out new_balance))
+                {
+                    pending_file.Records.Add(new CredCard2InOutRecord
+                    {
+                        Date = next_date,
+                        Description = ReconConsts.Cred_card2_regular_pymt_description,
+                        Unreconciled_amount = new_balance
+                    });
+                }
+                statement_date = next_date.AddMonths(-1);
+                next_date = next_date.Date.AddMonths(1);
+                input = input_output.Get_input(string.Format(
+                    ReconConsts.AskForCredCardDirectDebit,
+                    ReconConsts.Cred_card2_name,
+                    next_date.ToShortDateString()));
+            }
+
+            spreadsheet.Update_balance_on_totals_sheet(
+                Codes.Cred_card2_bal,
+                new_balance * -1,
+                string.Format(
+                    ReconConsts.CredCardBalanceDescription,
+                    ReconConsts.Cred_card2_name,
+                    $"{statement_date.ToString("MMM")} {statement_date.Year}"),
+                balance_column: 5,
+                text_column: 6,
+                code_column: 4);
+        }
     }
 }
