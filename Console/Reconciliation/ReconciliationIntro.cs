eb63271bbfb152b1a138d4a0b08bc822f4fc0fcc
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-11-27 16:22:11 +0000

    Remove unused function

--- Console/Reconciliation/ReconciliationIntro.cs
+++ Console/Reconciliation/ReconciliationIntro.cs
@@ -647,59 +647,6 @@ namespace ConsoleCatchall.Console.Reconciliation
         }
 
         public ReconciliationInterface
-            Load_bank_and_bank_out(
-                ISpreadsheet spreadsheet,
-                BudgetingMonths budgeting_months,
-                FilePaths main_file_paths)
-        {
-            var data_loading_info = BankAndBankOutData.LoadingInfo;
-            data_loading_info.File_paths = main_file_paths;
-
-            var pending_file_io = new FileIO<BankRecord>(_spreadsheet_factory);
-            var pending_file = new CSVFile<BankRecord>(pending_file_io);
-            pending_file_io.Set_file_paths(data_loading_info.File_paths.Main_path, data_loading_info.Pending_file_name);
-
-            _input_output.Output_line(ReconConsts.LoadingDataFromPendingFile);
-            // The separator we loaded with had to match the source. Then we convert it here to match its destination.
-            pending_file.Load(true, data_loading_info.Default_separator);
-            _input_output.Output_line("Converting source line separators...");
-            pending_file.Convert_source_line_separators(data_loading_info.Default_separator, data_loading_info.Loading_separator);
-            _input_output.Output_line(ReconConsts.MergingSomeBudgetData);
-            spreadsheet.Add_budgeted_bank_out_data_to_pending_file(
-                budgeting_months, 
-                pending_file, 
-                data_loading_info.Monthly_budget_data,
-                data_loading_info.Annual_budget_data);
-            _input_output.Output_line("Merging bespoke data with pending file...");
-            var file_loader = new FileLoader();
-            file_loader.Bank_and_bank_out__Merge_bespoke_data_with_pending_file(_input_output, spreadsheet, pending_file, budgeting_months, data_loading_info);
-            _input_output.Output_line("Updating source lines for output...");
-            pending_file.Update_source_lines_for_output(data_loading_info.Loading_separator);
-
-            // Pending file will already exist, having already been split out from phone Notes file by a separate function call.
-            // We loaded it up into memory in the previous file-specific method.
-            // Then some budget amounts were added to that file (in memory).
-            // Other budget amounts (like CredCard1 balance) were written directly to the spreadsheet before this too.
-            // Now we load the unreconciled rows from the spreadsheet and merge them with the pending and budget data.
-            // Then we write all that data away into the 'owned' csv file (eg BankOutPending.csv).
-            _input_output.Output_line("Merging unreconciled rows from spreadsheet with pending and budget data...");
-            spreadsheet.Add_unreconciled_rows_to_csv_file(data_loading_info.Sheet_name, pending_file);
-            _input_output.Output_line("Copying merged data (from pending, unreconciled, and budgeting) into main 'owned' csv file...");
-            pending_file.Write_to_file_as_source_lines(data_loading_info.File_paths.Owned_file_name);
-            _input_output.Output_line("...");
-
-            var third_party_file_io = new FileIO<ActualBankRecord>(_spreadsheet_factory, data_loading_info.File_paths.Main_path, data_loading_info.File_paths.Third_party_file_name);
-            var owned_file_io = new FileIO<BankRecord>(_spreadsheet_factory, data_loading_info.File_paths.Main_path, data_loading_info.File_paths.Owned_file_name);
-            var reconciliator = new BankReconciliator(third_party_file_io, owned_file_io, data_loading_info);
-            var reconciliation_interface = new ReconciliationInterface(
-                new InputOutput(),
-                reconciliator,
-                data_loading_info.Third_party_descriptor,
-                data_loading_info.Owned_file_descriptor);
-            return reconciliation_interface;
-        }
-
-        public ReconciliationInterface
             Load_cred_card1_and_cred_card1_in_out(
                 ISpreadsheet spreadsheet,
                 BudgetingMonths budgeting_months,
